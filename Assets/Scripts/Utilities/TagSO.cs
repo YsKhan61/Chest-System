5e24d965403643846fa0aa61aea431e3b646e62f
Yash Sadhukhan <yskhan61@gmail.com>
2024-05-26 13:06:08 +0530

    Added TagSO

---
+++ Assets/Scripts/Utilities/TagSO.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace CS.Utilities
+{
+    [CreateAssetMenu(fileName = "TagSO", menuName = "ScriptableObjects/TagSO", order = 0)]
+    public class TagSO : ScriptableObject
+    {
+    }
+}
